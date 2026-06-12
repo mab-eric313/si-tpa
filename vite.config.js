e186cec529efe26d05b5a57d664685079a25923e
mab-eric313 <kseneric313@gmail.com>
2026-06-12 22:53:34 +0700

    chore: Add Lucide dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	ssr: {
+		noExternal: ['lucide', 'bits-ui', 'svelte-sonner']
+	},
 	server: {
 		host: true,
 		proxy: {
