56a1c20979969802311909f8f587086e74e342db
mab-eric313 <kseneric313@gmail.com>
2026-07-22 08:51:45 +0700

    chore: Restructure project into frontend and backend dir

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -4,23 +4,26 @@ import adapter from '@sveltejs/adapter-auto';
 const config = {
 	compilerOptions: {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+		runes: ({ filename }) => 
+			filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 	},
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
+		/*
 		files: {
-			assets: 'frontend/static',
-			lib: 'frontend/src/lib',
-			routes: 'frontend/src/routes',
-			appTemplate: 'frontend/src/app.html',
+			assets: 'static',
+			lib: 'src/lib',
+			routes: 'src/routes',
+			appTemplate: 'src/app.html',
 			hooks: {
 				client: 'frontend/src/hooks.client',
 				server: 'frontend/src/hooks.server',
 			}
 		}
+		*/
 	}
 };
 
